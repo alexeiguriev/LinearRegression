829ef2585b1b0e0465572c958440b50ac8b3b1d8
Alexei <alexeiguriev1@gmail.com>
2024-04-20 00:31:58 +0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # LinearRegression
 Linear Regression
+
+1. To compile the application, can be used command "make"
+
+2. Then you can run the application on the command-line
+
+    for example: ./linear_regression 1 2 3 4 5 6 7 8 9 10 2 4 6 8 10 12 14 16 18 20
+    
+    This command line provides 10 pairs of x and y values, and the program will output the slope and intercept of the linear regression line.
--- main.c
+++ main.c
@@ -1,31 +1,17 @@
-// main.c
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "linear_regression.h"
 
 int main(int argc, char *argv[])
 {
-    if (argc < 6)
+    if (argc != 21)
     {
-        printf("Wrong parameters: %s\n", argv[0]);
-        return 1;
-    }
-    
-    int n = atoi(argv[1]);
-    if (((n % 2) != 0) || n <= 0) {
-        printf("Error: Invalid number of elements.\n");
+        printf("Usage: %s x1 y1 x2 y2 ... x10 y10\n", argv[0]);
         return 1;
     }
 
-    if (argc != (2 + (n * 2)))
-    {
-        printf("Wrong parameters: %s\n", argv[0]);
-        return 1;
-    }
-
-    double *dataX = malloc(n * sizeof(double));
-    double *dataY = malloc(n * sizeof(double));
+    double *dataX = malloc(10 * sizeof(double));
+    double *dataY = malloc(10 * sizeof(double));
 
     if (dataX == NULL || dataY == NULL)
     {
@@ -33,29 +19,16 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < 10; i++)
     {
-        dataX[i] = atof(argv[i * 2 + 2]);
-        dataY[i] = atof(argv[i * 2 + 3]);
+        dataX[i] = atof(argv[i * 2 + 1]);
+        dataY[i] = atof(argv[i * 2 + 2]);
     }
 
-    printf("DataX = ");
-    for (int index = 0; index < n; index++)
-    {
-        printf("%.2f ", dataX[index]);
-    }
-    
-    printf("\nDataY = ");
-    for (int index = 0; index < n; index++)
-    {
-        printf("%.2f ", dataY[index]);
-    }
+    LinearRegressionResult* result = LinearRegression(dataX, dataY, 10);
 
-    LinearRegressionResult* result = LinearRegression(dataX, dataY, n);
-
-    if (result != NULL)
-    {
-        printf("\nLinear Regression:\n");
+    if (result != NULL) {
+        printf("Linear Regression:\n");
         printf("Slope: %.2f\n", result->slope);
         printf("Intercept: %.2f\n", result->intercept);
         free(result);
@@ -66,3 +39,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
