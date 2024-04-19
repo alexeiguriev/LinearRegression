e01b14aca1899ac9507cf00acaca81c5a8d93000
Alexei <alexeiguriev1@gmail.com>
2024-04-20 00:20:40 +0300

    update

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if (argc < (2 + (n * 2)))
+    if (argc != (2 + (n * 2)))
     {
         printf("Wrong parameters: %s\n", argv[0]);
         return 1;
